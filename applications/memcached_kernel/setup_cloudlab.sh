5ab4dfa17c7ea33b96054334800356fad1039221
Nikita Lazarev <niklz@csail.mit.edu>
2023-06-25 19:00:31 +0000

    setup_cloudlab.sh small change

--- applications/memcached_kernel/setup_cloudlab.sh
+++ applications/memcached_kernel/setup_cloudlab.sh
@@ -1,8 +1,7 @@
-cd; sudo apt update
+# A script to prepare `d6515` machines in CloudLab for DPDK experiments
+# with Mellanox ConnectX-5 NICs.
 
-# Get DPDK.
-wget https://fast.dpdk.org/rel/dpdk-20.11.3.tar.gz
-tar -xvf dpdk-20.11.3.tar.gz
+cd; sudo apt update
 
 # Install dependencies.
 sudo apt install -y cmake
@@ -12,6 +11,10 @@ sudo apt install -y rdma-core
 sudo apt install -y libibverbs-dev
 sudo apt install -y libevent-dev
 sudo apt install -y libgflags-dev
+
+# Get DPDK.
+wget https://fast.dpdk.org/rel/dpdk-20.11.3.tar.gz
+tar -xvf dpdk-20.11.3.tar.gz
 
 # Build DPDK.
 cd dpdk-stable-20.11.3
